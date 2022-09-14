67f41c29216385abeb5d70d127a7e275e0ab9699
giusscos <giu.cosenza08@gmail.com>
2022-09-14 12:55:57 +0200

    Aggiunta Header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,8 +1,52 @@
 <template>
-    <header>
-        <h1>
-            Hello Proj!
-        </h1>
+    <header class="main_header">
+        <div class="header_wrapper container">
+            <div class="header-logo_wrapper">
+                <img src="../assets/img/logo_seo_w_1x.png" alt="logo" />
+            </div>
+            <nav class="header-main_nav">
+                <ul class="nav-list_wrapper">
+                    <li class="list_item">
+                        <a href="#" class="item_link capitalize">
+                            Home
+                        </a>
+                    </li>
+                    <li class="list_item">
+                        <a href="#" class="item_link capitalize">
+                            Who we are
+                        </a>
+                    </li>
+                    <li class="list_item">
+                        <a href="#" class="item_link capitalize">
+                            What we do
+                        </a>
+                    </li>
+                    <li class="list_item">
+                        <a href="#" class="item_link capitalize">
+                            Where we work
+                        </a>
+                    </li>
+                    <li class="list_item">
+                        <a href="#" class="item_link capitalize">
+                            Careers
+                            <span class="badge badge_primary uppercase">
+                                Apply
+                            </span>
+                        </a>
+                    </li>
+                    <li class="list_item">
+                        <a href="#" class="item_link capitalize">
+                            News
+                        </a>
+                    </li>
+                    <li class="list_item">
+                        <a href="#" class="btn btn_primary item_link uppercase">
+                            Get in touch now
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </header>
 </template>
 <script>
@@ -11,5 +55,34 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+@import '../style/index.scss';
 
+.main_header {
+    .header_wrapper {
+        display: flex;
+        padding: 1rem 0;
+        align-items: center;
+
+        .header-main_nav {
+            margin-left: auto;
+
+            .nav-list_wrapper {
+                gap: 1rem;
+                display: flex;
+
+                .list_item {
+                    display: flex;
+                    align-items: center;
+
+                    .item_link {
+
+                        &:hover {
+                            color: $pj-text_hover;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 </style>
