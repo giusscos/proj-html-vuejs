674018928634a723e3b28a73948dbc06654fff3d
giusscos <giu.cosenza08@gmail.com>
2022-09-14 22:11:56 +0200

    Refactoring Plans card

---
+++ src/components/PlanCardComponent.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="card_component">
+        <div class="card_wrapper" v-for="(plan,i) in plans" :key="`${i}plan`">
+            <h4 class="plan_category">
+                {{ plan.plan_category }}
+            </h4>
+            <div class="plan_price">
+                <span class="top">
+                    &dollar;
+                </span>
+                {{ plan.plan_price.split(',')[0].toString() }}
+                <span class="col_cent">
+                    <span class="top colored">
+                        {{ plan.plan_price.split(',')[1].toString() }}
+                    </span>
+                    <span class="bottom">
+                        monthly
+                    </span>
+                </span>
+            </div>
+            <p class="plan_info capitalize">
+                {{ plan.plan_project_quantity }} projects
+            </p>
+            <p class="plan_info capitalize">
+                {{ plan.plan_project_storage}} GB Storage
+            </p>
+            <p class="plan_info capitalize">
+                {{ plan.plan_project_users }} Users
+            </p>
+            <div class="btn_wrapper">
+                <a href="#" class="btn btn_border component_link uppercase">
+                    Start today
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+
+export default {
+    name: 'PlanCardComponent',
+    props: {
+        plans: Object,
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
--- src/components/PlansComponent.vue
+++ src/components/PlansComponent.vue
@@ -9,45 +9,13 @@
                     {{ desc }}
                 </p>
             </div>
-            <div class="card_component">
-                <div class="card_wrapper" v-for="(plan,i) in plans" :key="`${i}plan`">
-                    <h4 class="plan_category">
-                        {{ plan.plan_category }}
-                    </h4>
-                    <div class="plan_price">
-                        <span class="top">
-                            &dollar;
-                        </span>
-                        {{ plan.plan_price.split(',')[0].toString() }}
-                        <span class="col_cent">
-                            <span class="top colored">
-                                {{ plan.plan_price.split(',')[1].toString() }}
-                            </span>
-                            <span class="bottom">
-                                monthly
-                            </span>
-                        </span>
-                    </div>
-                    <p class="plan_info capitalize">
-                        {{ plan.plan_project_quantity }} projects
-                    </p>
-                    <p class="plan_info capitalize">
-                        {{ plan.plan_project_storage}} GB Storage
-                    </p>
-                    <p class="plan_info capitalize">
-                        {{ plan.plan_project_users }} Users
-                    </p>
-                    <div class="btn_wrapper">
-                        <a href="#" class="btn btn_border component_link uppercase">
-                            Start today
-                        </a>
-                    </div>
-                </div>
-            </div>
+            <PlanCardComponent :plans="plansCard"/>
         </div>
     </section>
 </template>
 <script>
+import PlanCardComponent from './PlanCardComponent.vue'
+
 const plansCard = [
     {
         plan_category: 'Standard',
@@ -85,11 +53,14 @@ export default {
         title: String,
         desc: String,
     },
+    components: {
+        PlanCardComponent,
+    },
     data() {
         return {
-            plans: plansCard,
+            plansCard
         }
-    }
+    },  
 }
 </script>
 <style lang="scss" scoped>
