e2d1a93b0714fe82286882847689d85bfc75a7a5
giusscos <giu.cosenza08@gmail.com>
2022-09-14 17:50:03 +0200

    Aggiunta terza sezione

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -2,19 +2,21 @@
     <main class="main_content">
         <JumboComponent />
         <InfoComponent 
-            :title="'Meet the new agency SEO template from the avada team'" 
-            :desc="'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro adipisci quas reiciendis animi ullam! Harum.'"
-        />
+            :title="'Meet the new agency SEO template from the avada team'"
+            :desc="'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro adipisci quas reiciendis animi ullam! Harum.'" />
         <ServicesComponent 
-            :title="'See our top notch services'" 
-            :desc="'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro adipisci quas reiciendis animi ullam! Harum.'"
-            />
+            :title="'See our top notch services'"
+            :desc="'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro adipisci quas reiciendis animi ullam! Harum.'" />
+        <WorksComponent 
+            :title="'Our work'"
+            :desc="'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro adipisci quas reiciendis animi ullam! Harum.'" />
     </main>
 </template>
 <script>
 import JumboComponent from './JumboComponent.vue'
-import InfoComponent from './InfoComponent.vue';
-import ServicesComponent from './ServicesComponent.vue';
+import InfoComponent from './InfoComponent.vue'
+import ServicesComponent from './ServicesComponent.vue'
+import WorksComponent from './WorksComponent.vue'
 
 export default {
     name: 'MainComponent',
@@ -22,6 +24,7 @@ export default {
         JumboComponent,
         InfoComponent,
         ServicesComponent,
+        WorksComponent,
     }
 }
 </script>
---
+++ src/components/WorksComponent.vue
@@ -0,0 +1,60 @@
+<template>
+    <section class="section works">
+        <div class="content container">
+            <div class="header_component">
+                <h2 class="component_title capitalize">
+                    {{ title }}
+                </h2>
+                <p class="component_desc">
+                    {{ desc }}
+                </p>
+            </div>
+            <div class="grid_component">
+                <div class="grid_item" v-for="(img,i) in imgs" :key="`${i}workImg`">
+                    <img :src="require(`../assets/img/${img}`)" :alt="`image-work-${i}`"/>
+                </div>
+            </div>
+            <div class="btn_wrapper">
+                <a class="btn btn_primary uppercase">
+                    View our work
+                </a>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+    name: 'WorksComponent',
+    props: {
+        title: String,
+        desc: String,
+    },
+    data(){
+        return {
+            imgs: [
+                'case-study-gallery-3-1.jpg',
+                'case-study-gallery-1-1.jpg',
+                'case-study-gallery-2-1.jpg',
+                'case-study-gallery-4-1.jpg',
+                'case-study-gallery-5-1.jpg',
+                'case-study-gallery-6-1.jpg',
+            ]
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+@import '../style/index.scss';
+.works{
+    padding-bottom: 5rem;
+
+    .grid_component{
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 1rem;
+    }
+    .btn_wrapper{
+        padding-top: 3rem;
+    }
+}
+</style>
